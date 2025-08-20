36ae190b7765ffd31182a18cdf83f7e05a18c52d
Avneet Kaur <avneetkaur87072@gmail.com>
2025-08-20 11:46:40 +0530

    Ques5.cpp

--- Ques5.cpp
+++ Ques5.cpp
@@ -63,103 +63,10 @@ void tridiagonal() {
 }
 
 
-
-void lowerTriangular() {
-    int n;
-    cout << "Enter size of lower triangular matrix: ";
-    cin >> n;
-
-    int size = n * (n + 1) / 2;  // only n(n+1)/2 elements will be non zero
-    int lower[size];
-
-    cout << "Enter " << size << " elements (row-wise): ";
-    for (int i = 0; i < size; i++) {
-        cin >> lower[i];
-    }
-
-    cout << "Lower Triangular Matrix:" << endl;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i >= j) {
-                int index = (i * (i + 1)) / 2 + j; 
-                cout << lower[index] << " ";
-            } else {
-                cout << "0 ";
-            }
-        }
-        cout << endl;
-    }
-}
-
-
-void upperTriangular() {
-    int n;
-    cout << "Enter size of upper triangular matrix: ";
-    cin >> n;
-
-    int size = n * (n + 1) / 2;  // only n(n+1)/2 elements needed
-    int upper[size];
-
-    cout << "Enter " << size << " elements (row-wise): ";
-    for (int i = 0; i < size; i++) {
-        cin >> upper[i];
-    }
-
-    cout << "Upper Triangular Matrix:" << endl;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i <= j) {
-                int index = (i * (2 * n - i + 1)) / 2 + (j - i);
-                cout << upper[index] << " ";
-            } else {
-                cout << "0 ";
-            }
-        }
-        cout << endl;
-    }
-}
-
-
-
-void symmetricMatrix() {
-    int n;
-    cout << "Enter size of symmetric matrix: ";
-    cin >> n;
-
-    int size = n * (n + 1) / 2;   // only n(n+1)/2 elements
-    int sym[size];
-
-    cout << "Enter " << size << " elements (lower triangular row-wise): ";
-    for (int i = 0; i < size; i++) {
-        cin >> sym[i];
-    }
-
-    cout << "Symmetric Matrix:" << endl;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i >= j) {
-                int index = (i * (i + 1)) / 2 + j;
-                cout << sym[index] << " ";
-            } else {
-                int index = (j * (j + 1)) / 2 + i; // using symmetry
-                cout << sym[index] << " ";
-            }
-        }
-        cout << endl;
-    }
-}
-
-
-
 int main(){
 	diagonal();
 	tridiagonal();
-	lowerTriangular();
-    upperTriangular();	
-    symmetricMatrix();  
 	return 0;
 }
+
 
