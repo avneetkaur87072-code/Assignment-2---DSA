d1cbf88d090ed3d4ff6ef09815ed6f24fddb9f9d
Avneet Kaur <avneetkaur87072@gmail.com>
2025-08-20 12:00:08 +0530

    Ques2.cpp

--- Ques2.cpp
+++ Ques2.cpp
@@ -5,12 +5,16 @@ int main()
     int arr[7] = {64,34,25,12,22,11,90};
     int n=7;
     for(int i=0; i<n; i++){
+        bool flag=false;
         for(int j=0; j<n-i-1; j++){
             if(arr[j] > arr[j+1] ){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                flag = true;
             }
+            if(!flag){
+             break;
         }
     }
     
@@ -21,3 +25,4 @@ int main()
 
     return 0;
 }
+
